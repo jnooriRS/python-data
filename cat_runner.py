d3ff58e11b372047252e304e32533890b57dd812
jnooriRS <jonas.noori@riversafe.co.uk>
2022-08-05 11:57:06 +0100

    classcreated with function

---
+++ cat_runner.py
@@ -0,0 +1,3 @@
+import Cat
+
+Cat.meow()
---
+++ catclass.py
@@ -0,0 +1,10 @@
+class Cat:
+    def __init__(self, name):
+        self.name = name
+    
+    def meow(self):
+        print("meow")
+    
+cat_1=Cat('Oreo')
+print(cat_1.name)
+cat_1.meow()
