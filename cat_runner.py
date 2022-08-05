7b49d8514b5d0db3f7251e2aac9a3b0c5d635cd4
jnooriRS <jonas.noori@riversafe.co.uk>
2022-08-05 15:01:01 +0100

    subclasses created with override function

--- cat_runner.py
+++ cat_runner.py
@@ -1,1 +1,9 @@
-import catclass
+from catclass import Cat, GoodCat, BadCat
+
+print('good cats go')
+Cat = GoodCat("Anthony")
+Cat.meow()
+
+print('bad cats go')
+Cat = BadCat ("Asad")
+Cat.meow()
--- catclass.py
+++ catclass.py
@@ -4,7 +4,24 @@ class Cat:
     
     def meow(self):
         print("meow")
+
+class GoodCat(Cat):
+    def __init__(self, name):
+         super().__init__(name)
+
+    def meow(self):     
+        print("purr purr meow")
     
+class BadCat(Cat):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def meow(self):
+        print("hiss")
+
+
+
+
 cat_1=Cat('Oreo')
 print(cat_1.name)
 cat_1.meow()
