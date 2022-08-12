f077df273f1cfd7779d060927123edf45841bcde
jnooriRS <jonas.noori@riversafe.co.uk>
2022-08-12 10:28:34 +0100

    fibonnaci return array

---
+++ async_multi/fibarry.py
@@ -0,0 +1,18 @@
+def fibArray(N):
+    for X in range(N):
+        fibarray=[]
+        num1 = 0
+        num2 = 1
+        find = 2
+        fibnum=0
+        
+        while find <= N:
+            fibnum = (num1+num2)
+            num1 = num2
+            num2 = fibnum
+            find = (find+1)
+            fibarray.append(int(fibnum))
+            continue
+    print(fibarray)
+
+fibArray(7) 
