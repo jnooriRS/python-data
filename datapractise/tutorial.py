18f1aad764d6ed41e8d8de665edb30b252acd97b
jnooriRS <jonas.noori@riversafe.co.uk>
2022-08-16 19:59:51 +0100

    removed api key will gen a new one

--- datapractise/tutorial.py
+++ datapractise/tutorial.py
@@ -2,7 +2,7 @@ import requests
 import json
 import os
 
-api_key = "cdb5c2c0542d5e5615a22f8f39194a0d"
+#change API 
 lat = "48.208176"
 lon = "16.373819"
 url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
