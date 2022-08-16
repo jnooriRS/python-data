9e58454efc984a021983b653d5a92d03d0844e9c
jnooriRS <jonas.noori@riversafe.co.uk>
2022-08-16 14:21:59 +0100

    input works for weather api and it makes the get request

---
+++ datapractise/tutorial.py
@@ -0,0 +1,10 @@
+import requests
+import json
+api_key = "b790ac9a284f6bec8f6dbd4ca520881e"
+lat = "48.208176"
+lon = "16.373819"
+url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
+
+response = requests.get(url)
+data = json.loads(response.text)
+print(data)
--- datapractise/weatherapp.py
+++ datapractise/weatherapp.py
@@ -1,5 +1,6 @@
-# OPENWEATHER 44ad1f7ca09faba5a87f676c8565dcde
+
 import requests
+import json
 #API for Open Weather
 api_key = "b790ac9a284f6bec8f6dbd4ca520881e"
 
@@ -7,11 +8,15 @@ user_input_latitude = input("Enter latitude: ")
 user_input_longitude = input("Enter longitude: ")
 
 #Check user inpt
-#print(user_input_latitude)
-#print(user_input_longitude)
+print(user_input_latitude)
+print(user_input_longitude)
 
+#Content type header- set to json in POSTMAN if inputting manually
 
 weather_data = requests.get(
-    f"https://api.openweathermap.org/data/2.5/weather?lat={user_input_latitude}&lon={user_input_longitude}&appid={api_key}"
-)
+    f"https://api.openweathermap.org/data/2.5/weather?lat={user_input_latitude}&lon={user_input_longitude}&appid={api_key}")
+
+data = json.loads(weather_data.text)
+print(data)
+
 print(weather_data.status_code)
