d3b19fdd29b6fd94ac9216b81c9fae5540b5f716
jnooriRS <jonas.noori@riversafe.co.uk>
2022-08-19 14:29:55 +0100

    login correct

--- datapractise/psycog_practise.py
+++ datapractise/psycog_practise.py
@@ -1,9 +1,10 @@
 # Small script to show PostgreSQL and Pyscopg together
-#
-
 import psycopg2
 
+conn = psycopg2.connect("dbname='XYZ user='postgres' host='localhost' password='XYZ!'")
+
 try:
-    conn = psycopg2.connect("dbname='template1' user='dbuser' host='localhost' password='dbpass'")
+   if conn:
+    print("Logged in")
 except:
     print("I am unable to connect to the database")
