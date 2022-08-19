46444789669e517a3ad8dc2292ee655dd7ac4620
jnooriRS <jonas.noori@riversafe.co.uk>
2022-08-19 15:06:20 +0100

    cursor conn is not working

--- datapractise/psycog_practise.py
+++ datapractise/psycog_practise.py
@@ -1,10 +1,17 @@
 # Small script to show PostgreSQL and Pyscopg together
 import psycopg2
+import psycopg2.extras
 
-conn = psycopg2.connect("dbname='XYZ user='postgres' host='localhost' password='XYZ!'")
+connection_to_db = psycopg2.connect("dbname='anthony_training' user='postgres' host='localhost' password='Rowing31!'")
 
 try:
-   if conn:
+   if connection_to_db:
     print("Logged in")
 except:
     print("I am unable to connect to the database")
+
+
+cur = conn.cursor()
+cur.execute("SELECT ** FROM weather ")
+
+#Good tutorial to follow
