0513add84d199732680e4f3734fa8689fbe99f66
jnooriRS <jonas.noori@riversafe.co.uk>
2022-08-23 15:02:48 +0100

    print databse

--- datapractise/psycog_practise.py
+++ datapractise/psycog_practise.py
@@ -2,7 +2,7 @@
 import psycopg2
 import psycopg2.extras
 
-connection_to_db = psycopg2.connect("dbname='anthony_training' user='postgres' host='localhost' password='Rowing31!'")
+connection_to_db = psycopg2.connect("dbname='XYZ' user='postgres' host='localhost' password='XYZ'")
 
 try:
    if connection_to_db:
@@ -11,7 +11,11 @@ except:
     print("I am unable to connect to the database")
 
 
-cur = conn.cursor()
-cur.execute("SELECT ** FROM weather ")
+cur = connection_to_db.cursor()
+cur.execute("SELECT * from weather ")
+rows = cur.fetchall()
+print ("\nShow me the databases:\n")
+for row in rows:
+    print("   ", row[0])
 
 #Good tutorial to follow
