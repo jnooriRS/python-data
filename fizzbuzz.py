3861405718ee3cbfddc0e528769712b643ffc645
jnooriRS <jonas.noori@riversafe.co.uk>
2022-08-23 12:46:08 +0100

    config file for automated black linting

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,13 @@
 for i in range(1, 21):
     msg = "Your current value is"
-    #print("Your current value is", i)
-    if i %3 == 0 and i %4 == 0:
+    # print("Your current value is", i)
+    if i % 3 == 0 and i % 4 == 0:
         print(f"{msg} FIZZBUZZ", i)
         continue
-    if i %3 == 0:
-     print( f" {msg} fizz", i)
-     continue
-    if i %4 == 0:
-     print(f" {msg} buzz", i)
-     continue
+    if i % 3 == 0:
+        print(f" {msg} fizz", i)
+        continue
+    if i % 4 == 0:
+        print(f" {msg} buzz", i)
+        continue
     print(msg, i)
